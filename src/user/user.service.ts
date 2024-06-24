419aec241e910bac04bef8e34e74172e04c78136
michaelYusak <michaelyusakt@gmail.com>
2024-06-25 02:00:40 +0700

    feat: add login end point

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,36 +1,45 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { DatabaseService } from 'src/database.service';
 import { ResultSetHeader } from 'mysql2';
-
-import bcrypt = require('bcrypt');
-const saltRounds = 10;
 
 @Injectable()
 export class UserService {
   constructor(private readonly dbService: DatabaseService) {}
 
-  async registerUser(userDTO: CreateUserDto) {
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hash = bcrypt.hashSync(userDTO.password, salt);
+  async registerUser(name: string, email: string, password: string) {
     const connection = this.dbService.getConnection();
 
     const [result] = await connection.query<ResultSetHeader>(
       'INSERT INTO users (user_name, user_email, user_password) VALUES (?, ?, ?);',
-      [userDTO.name, userDTO.email.toLowerCase(), hash],
+      [name, email.toLowerCase(), password],
     );
 
-    return { id: result.insertId, name: userDTO.name, email: userDTO.email };
+    return { id: result.insertId, name: name, email: email };
   }
 
   async findUser(userEmail: string): Promise<number> {
     const connection = this.dbService.getConnection();
 
     const [results] = await connection.execute(
-      'SELECT user_id FROM users WHERE user_email LIKE ?',
+      'SELECT user_id FROM users WHERE user_email = ?',
       [userEmail.toLowerCase()],
     );
 
+    if (!results[0]) {
+      return undefined;
+    }
+
     return results[0].user_id;
+  }
+
+  async getUserPassword(userId: number): Promise<string> {
+    const connection = this.dbService.getConnection();
+
+    const [results] = await connection.execute(
+      'SELECT user_password FROM users WHERE user_id = ?',
+      [userId],
+    );
+
+    return results[0].user_password;
   }
 }
